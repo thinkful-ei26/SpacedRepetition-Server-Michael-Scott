20ca6ce32dd5ee2b43395db35a916dd190966c2d
Scott Ian <sim2487@gmail.com>
2019-01-28 14:30:42 -0800

    questions route

---
+++ routes/questions.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Question = require('../models/question');
+
+const router = express.Router();
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+
+router.get('/', jwtAuth, (req, res, next) => {
+  const userId = req.user.id;
+  console.log("the user id is ", req.user.id);
+  Question.findOne()
+    .sort({ createdAt: 'desc' })
+    .then(sessions => {
+      res.json(sessions);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post('/', jwtAuth, (req, res, next) => {
+  const newQuestion = req.body;
+  const userId = req.user.id;
+  newQuestion.userId = userId;
+  console.log("the user id is ", userId);
+
+  Question.create(newQuestion)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+
+module.exports = router;
